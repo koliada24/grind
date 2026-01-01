c69299d1c87dae74a048132c3804591bcec83d37
koliada24 <olehkoliada37@gmail.com>
2026-01-02 00:48:18 +0200

    Added "lecture-8" materials

---
+++ cources/async-programming/lecture-8/SynchronizationOverview/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace SynchronizationOverview;
+
+public static class Program
+{
+    private static int Counter { get; set; }
+    
+    public static void Main()
+    {
+        var thread1 = new Thread(IncrementCounter);
+        var thread2 = new Thread(IncrementCounter);
+        
+        thread1.Start();
+        thread2.Start();
+        
+        thread1.Join();
+        thread2.Join();
+
+        Console.WriteLine(Counter);
+    }
+
+    private static void IncrementCounter()
+    {
+        for (var i = 0; i < 100000; i++)
+        {
+            var currentCounterValue = Counter;
+            var updatedCounterValue = currentCounterValue + 1;
+            Counter = updatedCounterValue;
+        }
+    }
+}
