e91b560af16cfafca2589c530ec0257d312520cb
koliada24 <olehkoliada37@gmail.com>
2026-01-14 00:59:46 +0200

    Course: Microservices. Implemented UpdateProduct feature

---
+++ cources/microservices/code/eshop-microservices/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductCommandHandler.cs
@@ -0,0 +1,44 @@
+﻿
+using JasperFx.Events.Daemon;
+
+namespace Catalog.API.Products.UpdateProduct
+{
+    public record UpdateProductCommand(
+        Guid Id,
+        string Name,
+        List<string> Categories,
+        string Description,
+        string ImageFile,
+        decimal Price
+    ) : ICommand<UpdateProductResult>;
+
+    public record UpdateProductResult(bool IsSuccess);
+
+    public class UpdateProductCommandHandler
+        (IDocumentSession session, ILogger<UpdateProductCommandHandler> logger)
+        : ICommandHandler<UpdateProductCommand, UpdateProductResult>
+    {
+        public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("UpdateProductCommandHandler.Handle called with {@Command}", command);
+
+            var product = await session.LoadAsync<Product>(command.Id);
+
+            if (product == null)
+            {
+                throw new ProductNotFoundException();   
+            }
+
+            product.Name = command.Name;
+            product.Description = command.Description;
+            product.Categories = command.Categories;
+            product.ImageFile = command.ImageFile;
+            product.Price = command.Price;
+
+            session.Update(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new UpdateProductResult(true);
+        }
+    }
+}
---
+++ cources/microservices/code/eshop-microservices/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductEndpoint.cs
@@ -0,0 +1,37 @@
+﻿
+namespace Catalog.API.Products.UpdateProduct
+{
+    public record UpdateProductRequest(
+        Guid Id,
+        string Name,
+        List<string> Categories,
+        string Description,
+        string ImageFile,
+        decimal Price
+    );
+
+    public record UpdateProductResponse(bool IsSuccess);
+
+    public class UpdateProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPut("/products", async (ISender sender, UpdateProductRequest request) =>
+            {
+                var command = request.Adapt<UpdateProductCommand>();
+
+                var result = await sender.Send(command);
+
+                var response = result.Adapt<UpdateProductResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("UpdateProduct")
+            .WithSummary("Update Product")
+            .WithDescription("Update Product")
+            .Produces<UpdateProductResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound);
+        }
+    }
+}
