22121f28054bf406226e34fe46b7bfc5f8eb342b
koliada24 <olehkoliada37@gmail.com>
2026-01-15 18:10:26 +0200

    Course: Microservices. Implemented pagination for GetProducts feature

--- cources/microservices/code/eshop-microservices/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsQueryHandler.cs
+++ cources/microservices/code/eshop-microservices/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsQueryHandler.cs
@@ -1,19 +1,20 @@
-﻿namespace Catalog.API.Products.GetProducts
+﻿using Marten.Pagination;
+
+namespace Catalog.API.Products.GetProducts
 {
-    public record GetProductsQuery
+    public record GetProductsQuery(int? PageNumber = 1, int? PageSize = 10)
         : IQuery<GetProductsQueryResult>;
 
     public record GetProductsQueryResult(IEnumerable<Product> Products);
     
 
-    internal class GetProductsQueryHandler(IDocumentSession session
-        , ILogger<GetProductsQueryHandler> logger)
+    internal class GetProductsQueryHandler(IDocumentSession session)
         : IQueryHandler<GetProductsQuery, GetProductsQueryResult>
     {
         public async Task<GetProductsQueryResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
         {
             var products = await session.Query<Product>()
-                .ToListAsync(cancellationToken);
+                .ToPagedListAsync(query.PageNumber ?? 1, query.PageSize ?? 10, cancellationToken);
 
             return new GetProductsQueryResult(products);
         }
--- cources/microservices/code/eshop-microservices/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductCommandHandler.cs
+++ cources/microservices/code/eshop-microservices/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductCommandHandler.cs
@@ -28,13 +28,11 @@ namespace Catalog.API.Products.UpdateProduct
     }
 
     public class UpdateProductCommandHandler
-        (IDocumentSession session, ILogger<UpdateProductCommandHandler> logger)
+        (IDocumentSession session)
         : ICommandHandler<UpdateProductCommand, UpdateProductResult>
     {
         public async Task<UpdateProductResult> Handle(UpdateProductCommand command, CancellationToken cancellationToken)
         {
-            logger.LogInformation("UpdateProductCommandHandler.Handle called with {@Command}", command);
-
             var product = await session.LoadAsync<Product>(command.Id);
 
             if (product == null)
