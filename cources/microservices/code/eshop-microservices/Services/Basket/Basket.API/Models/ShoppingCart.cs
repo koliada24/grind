252923aadd14e3ffc3f602eff0dfc511d4111cad
koliada24 <olehkoliada37@gmail.com>
2026-01-17 00:31:49 +0200

    Course: Microservices. Added domain entites to the Basket.API

---
+++ cources/microservices/code/eshop-microservices/Services/Basket/Basket.API/Models/ShoppingCart.cs
@@ -0,0 +1,19 @@
+﻿namespace Basket.API.Models
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; } = default!;
+        public List<ShoppingCartItem> Items { get; set; } = new();
+        public decimal TotalPrice => Items.Sum(item => item.Price * item.Quanity);
+
+        public ShoppingCart(string username)
+        {
+            UserName = username;
+        }
+
+        public ShoppingCart()
+        {
+
+        }
+    }
+}
