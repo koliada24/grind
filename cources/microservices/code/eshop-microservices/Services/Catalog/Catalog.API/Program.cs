bf4958c254c3adc4ff1bbb932508d512ec0c1c46
koliada24 <olehkoliada37@gmail.com>
2026-01-16 22:42:17 +0200

    Course: Microservices. Added health endpoint to Catalog.API

--- cources/microservices/code/eshop-microservices/Services/Catalog/Catalog.API/Program.cs
+++ cources/microservices/code/eshop-microservices/Services/Catalog/Catalog.API/Program.cs
@@ -1,3 +1,6 @@
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCarter();
@@ -24,10 +27,18 @@ if (builder.Environment.IsDevelopment())
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!);
+
 var app = builder.Build();
 
 app.MapCarter();
 
 app.UseExceptionHandler(options => { });
+
+app.UseHealthChecks("/health", new HealthCheckOptions
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
 
 app.Run();
