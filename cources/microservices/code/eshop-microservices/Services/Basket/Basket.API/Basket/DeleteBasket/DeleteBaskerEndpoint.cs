5ec6c346e0507a99d5bca8aa1776a22868bb99d1
koliada24 <olehkoliada37@gmail.com>
2026-01-17 00:42:00 +0200

    Course: Microservices. Added Endpoints and Handlers classes for all the features, added Carter nuget package to Basket.API

---
+++ cources/microservices/code/eshop-microservices/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBaskerEndpoint.cs
@@ -0,0 +1,10 @@
+﻿namespace Basket.API.Basket.DeleteBasket
+{
+    public class DeleteBaskerEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ cources/microservices/code/eshop-microservices/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoint.cs
@@ -0,0 +1,10 @@
+﻿namespace Basket.API.Basket.GetBasket
+{
+    public class GetBasketEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
