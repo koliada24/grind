c6fa11846f3bb5515d27fc3d4540b3e45d6f88a7
koliada24 <olehkoliada37@gmail.com>
2026-01-18 07:43:18 +0200

    Cource: Microservices. Implemented GetBasketEndpoint

--- cources/microservices/code/eshop-microservices/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoint.cs
+++ cources/microservices/code/eshop-microservices/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoint.cs
@@ -1,10 +1,28 @@
 ﻿namespace Basket.API.Basket.GetBasket
 {
+    public record GetBasketRequest(string UserName);
+
+    public record GetBasketResponse(ShoppingCart Cart);
+
     public class GetBasketEndpoint : ICarterModule
     {
         public void AddRoutes(IEndpointRouteBuilder app)
         {
-            throw new NotImplementedException();
+            app.MapGet("/basket/{userName}", async (string userName, ISender sender, CancellationToken cancellationToken) =>
+            {
+                var query = new GetBasketQuery(userName);
+
+                var result = await sender.Send(query, cancellationToken);
+
+                var response = result.Adapt<GetBasketResponse>();
+
+                return Results.Ok(response);
+            })
+            .Produces<GetBasketResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithName("GetBasket")
+            .WithSummary("Get Basket")
+            .WithDescription("Get Basket");
         }
     }
 }
--- cources/microservices/code/eshop-microservices/Services/Basket/Basket.API/GlobalUsing.cs
+++ cources/microservices/code/eshop-microservices/Services/Basket/Basket.API/GlobalUsing.cs
@@ -1,3 +1,5 @@
 ﻿global using Carter;
 global using Basket.API.Models;
 global using BuildingBlocks.CQRS;
+global using Mapster;
+global using MediatR;
