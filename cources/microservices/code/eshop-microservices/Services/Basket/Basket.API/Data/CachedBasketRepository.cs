ca4ccf0a7a3030502bbc4861a1ace82c110d1506
koliada24 <olehkoliada37@gmail.com>
2026-01-22 14:25:58 +0200

    Course: Microservices. Added Redis as a distributed cache to the Basket.API microservices, added Redis to docker-compose

---
+++ cources/microservices/code/eshop-microservices/Services/Basket/Basket.API/Data/CachedBasketRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+namespace Basket.API.Data
+{
+    public class CachedBasketRepository(IBasketRepository repository, IDistributedCache cache)
+        : IBasketRepository
+    {
+        public async Task<bool> DeleteBasketAsync(string userName, CancellationToken cancellationToken)
+        {
+            var dbDeletionResult = await repository.DeleteBasketAsync(userName, cancellationToken);
+
+            await cache.RemoveAsync(userName);
+
+            return dbDeletionResult;
+        }
+
+        public async Task<ShoppingCart> GetBasketAsync(string userName, CancellationToken cancellationToken)
+        {
+            var cachedSerializedBasket = await cache.GetStringAsync(userName, cancellationToken);
+
+            if (!string.IsNullOrEmpty(cachedSerializedBasket))
+            {
+                var cachedBasket = JsonSerializer.Deserialize<ShoppingCart>(cachedSerializedBasket);
+                return cachedBasket!;
+            }
+
+            var basket = await repository.GetBasketAsync(userName, cancellationToken);
+            await cache.SetStringAsync(userName, JsonSerializer.Serialize(basket), cancellationToken);
+            return basket;
+        }
+
+        public async Task<ShoppingCart> StoreBasketAsync(ShoppingCart basket, CancellationToken cancellationToken)
+        {
+            await repository.StoreBasketAsync(basket, cancellationToken);
+
+            await cache.SetStringAsync(basket.UserName, JsonSerializer.Serialize(basket), cancellationToken);
+
+            return basket;
+        }
+    }
+}
--- cources/microservices/code/eshop-microservices/Services/Basket/Basket.API/Program.cs
+++ cources/microservices/code/eshop-microservices/Services/Basket/Basket.API/Program.cs
@@ -3,18 +3,27 @@ using BuildingBlocks.Exceptions.Handlers;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCarter();
+
 builder.Services.AddMediatR(cfg =>
 {
     cfg.RegisterServicesFromAssemblyContaining<Program>();
     cfg.AddOpenBehavior(typeof(ValidationBehavior<,>));
     cfg.AddOpenBehavior(typeof(LoggingBehavior<,>));
 });
+
 builder.Services.AddMarten(options =>
 {
     options.Connection(builder.Configuration.GetConnectionString("Database")!);
     options.Schema.For<ShoppingCart>().Identity(x => x.UserName);
 }).UseLightweightSessions();
+
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+});
+
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 var app = builder.Build();
