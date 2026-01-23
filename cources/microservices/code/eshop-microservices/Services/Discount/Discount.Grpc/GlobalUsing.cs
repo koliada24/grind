c6d450744a9ada92257cb6976f33ad4c2383c806
koliada24 <olehkoliada37@gmail.com>
2026-01-23 03:52:29 +0200

    Course: Microservices. Added migration, fixed bugs, created discountdb

---
+++ cources/microservices/code/eshop-microservices/Services/Discount/Discount.Grpc/GlobalUsing.cs
@@ -0,0 +1,3 @@
+﻿global using Microsoft.EntityFrameworkCore;
+global using Discount.Grpc.Services;
+global using Discount.Grpc.Data;
--- cources/microservices/code/eshop-microservices/Services/Discount/Discount.Grpc/Program.cs
+++ cources/microservices/code/eshop-microservices/Services/Discount/Discount.Grpc/Program.cs
@@ -1,10 +1,13 @@
-using Discount.Grpc.Services;
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddGrpc();
+builder.Services.AddDbContext<DiscountContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("Database"));
+});
 
 var app = builder.Build();
+app.UseMigration();
 app.MapGrpcService<DiscountService>();
 
 app.Run();
