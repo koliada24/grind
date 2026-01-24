0eebdd0e14ceeb7638e1e65601732f0c388e4beb
koliada24 <olehkoliada37@gmail.com>
2026-01-25 01:32:12 +0200

    Course: Microservices. Added DependencyInjection classes to each layer, registered services in the Program.cs class

--- cources/microservices/code/eshop-microservices/Services/Ordering/Ordering.API/Program.cs
+++ cources/microservices/code/eshop-microservices/Services/Ordering/Ordering.API/Program.cs
@@ -1,4 +1,12 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAplicationServices();
+builder.Services.AddInfrastractureServices(builder.Configuration);
+builder.Services.AddApiServices();
 
 var app = builder.Build();
 
