ee0c0b6b23a1768110363e23d72a946eb346fd19
koliada24 <olehkoliada37@gmail.com>
2026-01-27 23:34:43 +0200

    Course: Microservices. Developed Strongly typed Ids for entities

--- cources/microservices/code/eshop-microservices/Services/Ordering/Ordering.Domain/Models/Product.cs
+++ cources/microservices/code/eshop-microservices/Services/Ordering/Ordering.Domain/Models/Product.cs
@@ -2,7 +2,7 @@
 
 namespace Ordering.Domain.Models
 {
-    public class Product : Entity<Guid>
+    public class Product : Entity<ProductId>
     {
         public string Name { get; private set; } = default!;
         public string Price { get; private set; } = default!;
---
+++ cources/microservices/code/eshop-microservices/Services/Ordering/Ordering.Domain/ValueObjects/OrderName.cs
@@ -0,0 +1,7 @@
+﻿namespace Ordering.Domain.ValueObjects
+{
+    public record OrderName
+    {
+        public string Value { get; } = string.Empty;
+    }
+}
