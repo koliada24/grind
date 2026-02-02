acccec2cb6bbb7a1a4f31cd01470c71723f26fdd
koliada24 <olehkoliada37@gmail.com>
2026-02-02 23:23:07 +0200

    Course: Microservices. Added ApplicationDbContext

---
+++ cources/microservices/code/eshop-microservices/Services/Ordering/Ordering.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿namespace Ordering.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
