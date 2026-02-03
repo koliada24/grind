ad11f1fc5cde30d73a416428d7bb762d53bdfd25
koliada24 <olehkoliada37@gmail.com>
2026-02-03 23:52:47 +0200

    Course: Microservices. Implemented OrderConfiguration

--- cources/microservices/code/eshop-microservices/Services/Ordering/Ordering.Infrastructure/GlobalUsing.cs
+++ cources/microservices/code/eshop-microservices/Services/Ordering/Ordering.Infrastructure/GlobalUsing.cs
@@ -4,3 +4,4 @@ global using Microsoft.EntityFrameworkCore.Metadata.Builders;
 global using Ordering.Domain.ValueObjects;
 global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
+global using Ordering.Domain.Enums;
