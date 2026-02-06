80fd5905910a4c3ce431a31b21c8e5ad4e700458
koliada24 <olehkoliada37@gmail.com>
2026-02-06 22:55:10 +0200

    Course: Microservices. Added automigration for OrderDB

--- cources/microservices/code/eshop-microservices/Services/Ordering/Ordering.API/Program.cs
+++ cources/microservices/code/eshop-microservices/Services/Ordering/Ordering.API/Program.cs
@@ -1,7 +1,3 @@
-using Ordering.API;
-using Ordering.Application;
-using Ordering.Infrastructure;
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddAplicationServices();
@@ -9,5 +5,12 @@ builder.Services.AddInfrastractureServices(builder.Configuration);
 builder.Services.AddApiServices();
 
 var app = builder.Build();
+
+app.UseApiServices();
+
+if (app.Environment.IsDevelopment())
+{
+    await app.InitialiseDatabaseAsync();
+}
 
 app.Run();
