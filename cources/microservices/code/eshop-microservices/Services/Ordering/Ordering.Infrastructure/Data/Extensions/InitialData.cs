3a63c5392ced4e1d46ff277d2333c7526d74f1cd
koliada24 <olehkoliada37@gmail.com>
2026-02-10 19:21:09 +0200

    Course: Microservices. Fixed identation

--- cources/microservices/code/eshop-microservices/Services/Ordering/Ordering.Infrastructure/Data/Extensions/InitialData.cs
+++ cources/microservices/code/eshop-microservices/Services/Ordering/Ordering.Infrastructure/Data/Extensions/InitialData.cs
@@ -3,11 +3,11 @@
 internal class InitialData
 {
     public static IEnumerable<Customer> Customers =>
-    new List<Customer>
-    {
-        Customer.Create(CustomerId.Of(new Guid("58c49479-ec65-4de2-86e7-033c546291aa")), "mehmet", "mehmet@gmail.com"),
-        Customer.Create(CustomerId.Of(new Guid("189dc8dc-990f-48e0-a37b-e6f2b60b9d7d")), "john", "john@gmail.com")
-    };
+        new List<Customer>
+        {
+            Customer.Create(CustomerId.Of(new Guid("58c49479-ec65-4de2-86e7-033c546291aa")), "mehmet", "mehmet@gmail.com"),
+            Customer.Create(CustomerId.Of(new Guid("189dc8dc-990f-48e0-a37b-e6f2b60b9d7d")), "john", "john@gmail.com")
+        };
 
     public static IEnumerable<Product> Products =>
         new List<Product>
