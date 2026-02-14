6d6a79fdaa203b738a0b6cd5acfa289f85ca366f
koliada24 <olehkoliada37@gmail.com>
2026-02-14 16:10:23 +0200

    Course: Microservices, lecture-246. Implemented GetOrders usecase with pagination

---
+++ cources/microservices/code/eshop-microservices/Services/Ordering/Ordering.Application/Orders/Queries/GetOrders/GetOrdersQuery.cs
@@ -0,0 +1,8 @@
+﻿using BuildingBlocks.Pagination;
+
+namespace Ordering.Application.Orders.Queries.GetOrders
+{
+    public record GetOrdersQuery(PaginationRequest PaginationRequest) : IQuery<GetOrdersResult>;
+
+    public record GetOrdersResult(PaginationResult<OrderDto> PaginationResult);
+}
